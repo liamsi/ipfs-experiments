a7636b9badf6ff9be68864e1e3f226b5b9979343
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-11-25 14:08:15 +0100

    256

--- dht_lookup_time/experiment_dag.py
+++ dht_lookup_time/experiment_dag.py
@@ -12,18 +12,18 @@ ipfs = ipfshttpclient.connect()
 
 def run_test(i):
     # TODO it would be better to generate these testfiles in python instead:
-    res = ipfs.dag.put(data='testfiles/32_'+str(i)+'.json', format='merkle-leaves')
+    res = ipfs.dag.put(data='testfiles/256_'+str(i)+'.json', format='merkle-leaves')
     cid = str(res["Cid"]["/"])
     paths = ""
 
     num_paths = 15
     for idx in range(0, num_paths):
-        path_num = random.randint(0, 31)
+        path_num = random.randint(0, 256)
         if idx != 0:
             path = "+"
         else:
             path = ""
-        path = path + cid + "/" + "/".join([char for char in '{0:05b}'.format(path_num)])
+        path = path + cid + "/" + "/".join([char for char in '{0:08b}'.format(path_num)])
         print(path)
         paths = paths+path
 
@@ -39,5 +39,5 @@ for i in range(100):
 
 plt.hist(x)
 plt.ylabel('Probability')
-plt.xlabel('Latency (ms)');
-plt.savefig('graph-dag-full.pdf')
+plt.xlabel('Latency (ms)')
+plt.savefig('graph-dag-256.pdf')
