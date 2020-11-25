1299b72126987d1bd0dd3ce7e4c8ce802e82f883
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-11-25 13:10:30 +0100

    para

--- dht_lookup_time/experiment_dag.py
+++ dht_lookup_time/experiment_dag.py
@@ -10,18 +10,21 @@ import matplotlib.pyplot as plt
 current_milli_time = lambda: int(round(time.time() * 1000))
 ipfs = ipfshttpclient.connect()
 
-def run_test():
-
-    res = ipfs.dag.put(data='testfiles/32.json', format='merkle-leaves')
+def run_test(i):
+    # TODO it would be better to generate these testfiles in python instead:
+    res = ipfs.dag.put(data='testfiles/32_'+str(i)+'.json', format='merkle-leaves')
     cid = str(res["Cid"]["/"])
     paths = ""
-    for idx in range(0, 15):
+
+    num_paths = 15
+    for idx in range(0, num_paths):
         path_num = random.randint(0, 31)
         if idx != 0:
             path = "+"
         else:
             path = ""
         path = path + cid + "/" + "/".join([char for char in '{0:05b}'.format(path_num)])
+        print(path)
         paths = paths+path
 
     start_time = current_milli_time()
@@ -30,11 +33,11 @@ def run_test():
 
 x = []
 for i in range(100):
-    t = run_test()
+    t = run_test(i)
     print(t)
     x.append(t)
 
 plt.hist(x)
 plt.ylabel('Probability')
 plt.xlabel('Latency (ms)');
-plt.savefig('graph2.pdf')
+plt.savefig('graph-dag-full.pdf')
--- dht_lookup_time/server.py
+++ dht_lookup_time/server.py
@@ -2,7 +2,7 @@ import time
 import urllib
 
 from flask import Flask
-
+import concurrent.futures
 import ipfshttpclient
 
 current_milli_time = lambda: int(round(time.time() * 1000))
@@ -21,9 +21,16 @@ def receive_hash(ipfs_hash):
 def get_dag_paths(paths):
     paths = urllib.parse.unquote(paths)
     paths = paths.split('+')
-    for path in paths:
-        # TODO run multiple dag gets in parallel
-        ipfs.dag.get(path)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
+        future_to_path = {executor.submit(ipfs.dag.resolve, path): path for path in paths}
+        for future in concurrent.futures.as_completed(future_to_path):
+            path = future_to_path[future]
+            try:
+                data = future.result()
+            except Exception as exc:
+                print('%r generated an exception: %s' % (path, exc))
+            else:
+                print('%r path resolved %s' % (path, len(data)))
 
     return str(current_milli_time())
 
